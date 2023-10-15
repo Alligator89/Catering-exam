3c39ea41a418476ccb53dd3de951eee119095e50
Alligator89 <denisliubenkov@yandex.by>
2023-10-15 12:32:19 +0300

    Creating security JwtAuthenticationFilter

---
+++ src/main/java/com/exam/catering/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.exam.catering.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+
+    private final CustomUserDetailService customUserDetailService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = jwtUtils.getTokenFromHttpRequest(request);
+
+        if (token != null && jwtUtils.validateToken(token)) {
+            String login = jwtUtils.getLoginFromJwt(token);
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(login);
+
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
