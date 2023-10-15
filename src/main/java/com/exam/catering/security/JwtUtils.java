7e1dbe99e36185ee1233e7648152107be245dfea
Alligator89 <denisliubenkov@yandex.by>
2023-10-15 12:38:00 +0300

    Creating security JwtUtils

---
+++ src/main/java/com/exam/catering/security/JwtUtils.java
@@ -0,0 +1,54 @@
+package com.exam.catering.security;
+
+import io.jsonwebtoken.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateJwtToken(String login) {
+        return Jwts.builder()
+                .setSubject(login)
+                .setExpiration(new Date(new Date().getTime() + expiration))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+        } catch (ExpiredJwtException e) {
+        } catch (UnsupportedJwtException e) {
+        } catch (IllegalArgumentException e) {
+        }
+        return false;
+    }
+
+    public String getTokenFromHttpRequest(HttpServletRequest request) {
+        final String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String getLoginFromJwt(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+}
