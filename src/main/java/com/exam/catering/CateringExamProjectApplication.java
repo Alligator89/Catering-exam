82a31a30bd4d3217f3ff3dd132ccbfd4ce795eb1
Alligator89 <denisliubenkov@yandex.by>
2023-10-15 10:46:04 +0300

    Setting of pom.xml file

---
+++ src/main/java/com/exam/catering/CateringExamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.exam.catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringExamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringExamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam/catering/CateringExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.catering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringExamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
